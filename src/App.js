98ff9b59e4ed6039c50aa29af2c7eaace2c53a21
unknown <nefaa1999@gmail.com>
2024-04-29 13:15:03 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react' ; 
+import  {useEffect , useState} from 'react' ; 
+import Recipe from './Recipe'; 
 
-function App() {
+import './App.css' ; 
+
+
+
+const  App = () =>  { 
+  const [recipes , SetRecipes]= useState([]) ;  
+  const [search ,SetSearch] = useState("") ;  
+  const APP_ID = '5241c805';   
+  const APP_KEY = 'b44f450ba8934eb947ecab8d21df6958' ;  
+  
+ 
+  const [ query ,  setQuery] = useState("chicken ") ; 
+
+  useEffect (()=> {  
+    getRecipes() ;  
+
+  }, query) 
+
+  const getRecipes = async () => {  
+    const response = await fetch 
+    (`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`) ;  
+    const data = await response.json() ; 
+    SetRecipes(data.hits) ; 
+
+    console.log(data)  ;  
+
+  } 
+
+  const updateSearch = e => { 
+    SetSearch(e.target.value); 
+  }; 
+  const getSearch = e => { 
+    e.preventDefault(); 
+    setQuery(search); 
+    SetSearch(""); 
+  } 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <form className='search-form' onSubmit={getSearch}> 
+          <input className='search-bar' type='text' value={search} onChange={updateSearch} />
+             <button className='search-button' type='submit' > Search </button>             
+             
+          </form>   
+
+          <div className="recipes"> 
+             <Recipe/>
+  
+      
+      </div>
+              
     </div>
   );
 }
